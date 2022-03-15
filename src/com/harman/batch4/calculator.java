63fdfb7d0773628dd7b8a4c6c9ac2a296f91c095
via2121 <virajadhav2121.gmail.com>
2022-03-15 15:54:29 +0530

    Addition operation with function completed

--- src/com/harman/batch4/calculator.java
+++ src/com/harman/batch4/calculator.java
@@ -1,7 +1,13 @@
 package com.harman.batch4;
 import java.util.Scanner;
 public class calculator {
+    public int AdditionOperation(int x, int y)
+    {
+        int z=x+y;
+        return z;
+    }
     public static void main(String[] args){
+        calculator calc=new calculator(); //object creation
         Scanner input=new Scanner(System.in);
         int n1,n2,result;
         System.out.println("Enter num1");
